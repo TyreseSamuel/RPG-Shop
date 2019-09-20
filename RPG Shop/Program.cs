1abd93442eb0efe41e65a657f84e08d537e9afc4
Tyrese Samuel <3tyrese@gmail.com>
2019-09-20 10:37:45 -0500

    Add project files.

---
+++ RPG Shop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Shop
+{
+    class Program
+    {
+        static float gold = 0.00f;
+        static string choice = "";
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
