eb96b96fe30634460c9588f2f649e0e24610a22b
Tyrese Samuel <3tyrese@gmail.com>
2019-09-25 17:52:22 -0500

    Start of project

--- RPG Shop/Program.cs
+++ RPG Shop/Program.cs
@@ -12,7 +12,16 @@ namespace RPG_Shop
         static string choice = "";
         static void Main(string[] args)
         {
-            
+            string choice = "";
+            while (choice != "0")
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1: Add Gold");
+                Console.WriteLine("2: Buy Bulma");
+                Console.WriteLine("3: Buy Icarus");
+                Console.WriteLine("4: Buy Senzu Bean");
+                Console.WriteLine("5: Exit");
+            }
         }
     }
 }
